c5d271946ff3b32969b17ca3f3d706d6d9519719
nobodyme <nobodydisturbme@gmail.com>
2025-12-06 16:52:26 +0530

    added day 4 part-2

---
+++ 2025/day-4/part-2/solution.py
@@ -0,0 +1,47 @@
+def rolls_accessible(paper_rolls):
+
+    free = set()
+    ROWS = len(paper_rolls)
+    COLS = len(paper_rolls[0])
+    directions = [(0,-1), (0,1), (-1,0), (1,0), (-1,-1), (-1,1), (1,-1), (1,1)]
+
+    prev_total, total = -1, 0
+
+    while prev_total != total:
+        print(f'prev - {prev_total} and total - {total}')
+        prev_total = total
+        for r in range(ROWS):
+                for c in range(COLS):
+                    occupied_count = 0
+
+                    if paper_rolls[r][c] == "@":
+                        for direction in directions:
+                            
+                            new_row, new_col = r + direction[0], c + direction[1]
+                            if new_row < 0 or new_col < 0 or new_row >= ROWS or new_col >= COLS:
+                                continue
+                            
+                            if paper_rolls[new_row][new_col] == "@":
+                                occupied_count += 1
+                            
+                            if occupied_count >= 4:
+                                break
+                    
+                        if occupied_count < 4:
+                            # TODO: This can be optimized
+                            s = list(paper_rolls[r])
+                            s[c] = 'X'
+                            paper_rolls[r] = "".join(s)
+                            free.add((r, c))
+                            total += 1
+
+        print("\n\n")
+    return total
+
+input_file = '../input.txt'
+paper_rolls = []
+with open(input_file) as f:
+    paper_rolls = f.read().splitlines()
+
+accessible_rolls = rolls_accessible(paper_rolls)
+print("total", accessible_rolls)
